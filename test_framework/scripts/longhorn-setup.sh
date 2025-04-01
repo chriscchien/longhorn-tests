50092255f0115b438e77ab0ece61be4a03a2cac1
Chris Chien <chris.chien@suse.com>
2025-04-01 10:10:34 +0800

    123

--- test_framework/scripts/longhorn-setup.sh
+++ test_framework/scripts/longhorn-setup.sh
@@ -99,7 +99,6 @@ main(){
   # msg="failed to get package manager" error="operating systems amzn are not supported"
   if [[ "${TF_VAR_k8s_distro_name}" != "eks" ]] && \
     [[ "${DISTRO}" != "talos" ]]; then
-    sleep 300s
     longhornctl_check
   fi
 
