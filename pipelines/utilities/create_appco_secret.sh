c36628a3f0667447c6551378339761bdbec05574
Chris Chien <chris.chien@suse.com>
2025-05-07 15:33:12 +0800

    first commit

--- pipelines/utilities/create_appco_secret.sh
+++ pipelines/utilities/create_appco_secret.sh
@@ -1,4 +1,9 @@
 create_appco_secret(){
+    set +x
     kubectl -n default create secret docker-registry application-collection --docker-server=dp.apps.rancher.io --docker-username="${APPCO_USERNAME}" --docker-password="${APPCO_PASSWORD}"
     kubectl -n longhorn-system create secret docker-registry application-collection --docker-server=dp.apps.rancher.io --docker-username="${APPCO_USERNAME}" --docker-password="${APPCO_PASSWORD}"
+    set +x
+
+    kubectl patch serviceaccount default --type='merge' -p '{"imagePullSecrets":[{"name":"application-collection"}]}' -n default
+    kubectl patch serviceaccount default --type='merge' -p '{"imagePullSecrets":[{"name":"application-collection"}]}' -n longhorn-system    
 }
