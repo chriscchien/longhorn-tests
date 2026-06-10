7806a4a8d06d332dc5a626ac8584bdc2e2076873
Chris Chien <chris.chien@suse.com>
2026-06-10 17:17:47 +0800

    123

--- pipelines/utilities/run_longhorn_e2e_test.sh
+++ pipelines/utilities/run_longhorn_e2e_test.sh
@@ -222,7 +222,7 @@ run_longhorn_test_out_of_cluster(){
   elif [[ $BACKUP_STORE_TYPE = "azurite" ]]; then
     LONGHORN_BACKUPSTORES=${AZURITE_BACKUP_STORE}
   fi
-  LONGHORN_BACKUPSTORE_POLL_INTERVAL="30s"
+  LONGHORN_BACKUPSTORE_POLL_INTERVAL="30"
 
   if [[ "${CUSTOM_TEST_OPTIONS}" == \"*\" ]]; then
     # Remove leading and trailing double quotes
